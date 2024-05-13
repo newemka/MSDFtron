71d64a93a238ac0e92bb735365b127f56edc2830
newemka <newemka@gmail.com>
2024-05-13 02:36:45 +0200

    customizable charset is working

--- src/render.js
+++ src/render.js
@@ -2,32 +2,29 @@ const form = document.getElementById('myForm');
 const font = document.querySelector('#font');
 const filename = document.querySelector('#filename');
 const outputPath = document.querySelector('#output-path');
-//options
 
+const charedit = document.querySelector('#charedit');
+//options
 const fontSize = document.querySelector('#FontSize');
 const distanceRange = document.querySelector('#DistanceRange');
 const texturePadding = document.querySelector('#TexturePadding');
 const textureSize = document.querySelector('#TextureSize');
-const charedit = document.querySelector('#charedit');
+
 // Assume you have a reference to the HTML element where you want to display the message
 const messageElement = document.getElementById('message');
 const elemI = document.querySelector("#FeedBack");
 const feedbackStyle = 'selected';
 
-let charset; // Declare charset variable outside the scope of the promise chain
-let contented;
-charsetter.char().then(content => {
-    console.log(content);
-    charset = content; // Assign content to charset inside the promise chain
-    return contented;
-}).catch(error => {
-    console.error('Error fetching file content:', error);
-    charset = ''; // Assign an empty string to charset in case of error
-});
-
-
 function loadFont(e) {
     const file = e.target.files[0];
+
+    charsetter.char().then(content => {
+        console.log(content);
+        charedit.value = content; // Assign content to charedit inside the promise chain
+    }).catch(error => {
+        console.error('Error fetching file content:', error);
+        charedit.value = ''; // Assign an empty string to charedit in case of error
+    });
 
     /*  if (!isFileFont(file)) {
          console.log('Please select a font');
@@ -39,7 +36,7 @@ function loadFont(e) {
     filename.innerHTML = font.files[0].path;
     //outputPath.innerHTML = os.homedir();
     //outputPath.innerHTML = path.join(os.homedir(), 'MsdfFonts')
-    charedit.innerText = contented;
+
     elemI.className = feedbackStyle;
     elemI.innerHTML = 'font selected ✓';
 }
@@ -48,7 +45,7 @@ function loadFont(e) {
 function sendFont(e) {
 
     e.preventDefault();
-    const charsetpathvalue = charset;
+    const charsetpathvalue = charedit.value;
     const fontSizeValue = fontSize.value;
     const fontPath = font.files[0].path;
     const distanceRangeValue = distanceRange.value;
